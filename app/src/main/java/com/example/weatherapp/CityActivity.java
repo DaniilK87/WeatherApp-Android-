a4cdcce5d7d88a5fb99de268aa4d9b40b468eeb6
Daniil <denko@list.ru>
2020-07-02 15:16:24 +0500

    homework 1

--- app/src/main/java/com/example/weatherapp/CityActivity.java
+++ app/src/main/java/com/example/weatherapp/CityActivity.java
@@ -1,19 +1,14 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class CityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onMyClick(View view) {
-
+        setContentView(R.layout.activity_city);
     }
 }
